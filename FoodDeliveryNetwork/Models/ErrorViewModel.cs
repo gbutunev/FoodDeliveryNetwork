9ab164e3a3d0b1cac2179969e4da31373bc97b80
georg <georg@OP7020>
2023-07-24 21:54:45 +0300

    Add project files.

---
+++ FoodDeliveryNetwork/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodDeliveryNetwork.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
