986a554aa6164d005592d26d4574a357f067065a
gbutunev <78198180+gbutunev@users.noreply.github.com>
2023-08-11 18:25:12 +0300

    err pages

--- FoodDeliveryNetwork/Program.cs
+++ FoodDeliveryNetwork/Program.cs
@@ -40,7 +40,7 @@ namespace FoodDeliveryNetwork.Web
             builder.Services.AddControllersWithViews(options =>
             {
                 options.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
-                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());                
+                options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute());
             });
 
             builder.Services.AddServerSideBlazor().AddCircuitOptions(o =>
@@ -72,7 +72,8 @@ namespace FoodDeliveryNetwork.Web
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Home/Error/500");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
